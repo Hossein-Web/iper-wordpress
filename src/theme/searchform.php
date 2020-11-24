50dd26f532cf629d48f54ecf6e197b636e930d76
akram-web <email3307@gmail.com>
2020-11-24 11:37:03 +0330

    bug in search form was modified

--- src/theme/searchform.php
+++ src/theme/searchform.php
@@ -1,6 +1,6 @@
-<form class="search__form" role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+<form class="search__form" role="search" method="get" action="<?php echo home_url( '/' ); ?>">
 	<div class="inner-content">
-		<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="<?php _e( 'جستجو', 'persian_bourse' ); ?>" />
+		<input type="text" value="<?php echo get_search_query(); ?>" name="s" placeholder="<?php _e( 'جستجو', 'persian_bourse' ); ?>" />
         <button type="submit"></button>
 	</div>
 </form>
